8843d6163d2e6e2aa9c73aec956c8022292caa6c
PiotrCharkot <piotr.charkot@gmail.com>
2024-07-31 15:24:48 +0200

    links added

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ window.addEventListener('scroll', () => {
         backgroundLogo.style.opacity = 0;
     }
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sliders = document.querySelectorAll('.image-slider');
+
+    sliders.forEach(slider => {
+        let currentIndex = 0;
+        const images = slider.querySelectorAll('img');
+        const totalImages = images.length;
+
+        // Show the first image
+        images[currentIndex].classList.add('active');
+
+        setInterval(() => {
+            images[currentIndex].classList.remove('active');
+            currentIndex = (currentIndex + 1) % totalImages;
+            images[currentIndex].classList.add('active');
+        }, 4500); // Change image every 3 seconds
+    });
+});
